a24edcb96e1fb50ea40954d9428ad51989f5dcd1
FranMulti1200 <fbautista848stf@gmail.com>
2024-12-11 12:33:14 +0100

    Day 42: Used inheritance and overwrite with class

--- main_exc.py
+++ main_exc.py
@@ -1,10 +1,8 @@
 import pandas
-from matplotlib.pyplot import connect
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 df_cards_security = pandas.read_csv("card_security.csv", dtype=str)
-
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -17,12 +15,17 @@ class Hotel:
         df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        "Check if the hotel is available"
+        """Check if the hotel is available"""
         availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
         if availability == "yes":
             return True
         else:
             return False
+
+
+class SpaHotel(Hotel):
+    def book_spa_package(self):
+        pass
 
 
 class ReservationTicket:
@@ -40,18 +43,6 @@ class ReservationTicket:
         return content
 
 
-class ReservationSpa(ReservationTicket):
-    def generate(self):
-        content = f"""
-        Thank you for your SPA reservation!
-        Here are you SPA booking data:
-        Name: {self.customer_name}
-        Hotel name: {self.hotel.name}
-        """
-        return content
-
-
-
 class CreditCard:
     def __init__(self, number):
         self.number = number
@@ -64,7 +55,7 @@ class CreditCard:
         else:
             return False
 
-"""Hereda de la clase CreditCard"""
+
 class SecureCreditCard(CreditCard):
     def authenticate(self, given_password):
         password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
@@ -74,28 +65,41 @@ class SecureCreditCard(CreditCard):
             return False
 
 
+class SpaTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your SPA reservation!
+        Here are you SPA booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
 
 print(df)
-hotel_id = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_id)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = SpaHotel(hotel_ID)
 
 if hotel.available():
-    #card_number = input("Enter the credit card number")
     credit_card = SecureCreditCard(number="1234567890123456")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
         if credit_card.authenticate(given_password="mypass"):
             hotel.book()
             name = input("Enter your name: ")
-            spa_hotel = input("Do you want to book a spa package?")
-            if spa_hotel == "no":
-                reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-                print(reservation_ticket.generate())
-            else:
-                reservationSpa_ticket = ReservationSpa(customer_name=name, hotel_object=hotel)
-                print(reservationSpa_ticket.generate())
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+            spa = input("Do you want to book a spa package? ")
+            if spa == "yes":
+                hotel.book_spa_package()
+                spa_ticket = SpaTicket(customer_name=name, hotel_object=hotel)
+                print(spa_ticket.generate())
         else:
             print("Credit card authentication failed.")
     else:
         print("There was a problem with your payment")
 else:
-    print("Hotel is not free")
+    print("Hotel is not free.")
